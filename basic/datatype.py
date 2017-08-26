2fb361be3b7566622fe2ca66fe73f7cbfb652858
cangxue <752787086@qq.com>
2017-08-26 23:27:13 +0800

    基础变量及操作学习

--- basic/datatype.py
+++ basic/datatype.py
@@ -1,14 +1,39 @@
-# codin: utf-8
-
+# escape character
 print('I\'m \"ok\"!')
-
-
 print('\\\n\\')
 print('\\\t\\')
 print(r'\\\t\\')
 
+# bool
 print(3 > 2)
 print(5 > 3 and 3 > 2)
 print(5 > 3 or 3 > 4)
 print(not 1 > 2)
+
+# None
+
+# variable
+a = 10
+t_007 = 'T007'
+Answer = True
+print(a, t_007, Answer)
+
+x = 10
+x = x + 2
+print(x)
+
+b = a
+a = 20
+print(a, b)
+
+#通常用全部大写的变量名表示常量
+PI = 3.14159265359
+
+#除法
+#一种除法是/：
+print(10/3, 9/3)
+#还有一种除法是//，称为地板除，两个整数的除法仍然是整数：
+print(10 // 3)
+print(10 % 3)
+
 
