983592ce3c61e32a3f00736b381d16d43b7de3c3
cangxue <752787086@qq.com>
2017-08-28 15:19:05 +0800

    随机函数学习及pi的计算方法实现

--- basic/math.py
+++ basic/math.py
@@ -1,5 +1,8 @@
-#数学库
+#数学库math
 from math import *
+from random import *
+from time import clock
+
 print(pi)
 
 f = pi
@@ -27,3 +30,33 @@ print(d1, d2, d3, d4, d5, d6, d7, d8, d9)
 #54.598150033144236 229.1831180523293 0.06981317007977318 -0.3048106211022167
 #-0.9524129804151563 0.3200403893795629 1.5707963267948966 0.0 0.7853981633974483
 
+#随机数库random
+seed(10)
+r1 = random()
+r2 = uniform(0, 100)
+r3 = randint(0, 100)
+r4 = randrange(0, 100, 5)
+ra = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+r5 = choice(ra)
+print(r1, r2, r3, r4, r5)
+#0.5714025946899135 42.888905467511464 73 0 3
+
+r6 = shuffle(ra)
+print(ra)
+#[2, 8, 6, 3, 0, 1, 5, 4, 9, 7]
+
+r7 = sample(ra, 5)
+print(r7)
+#[3, 1, 2, 7, 8]
+
+DARTS = 1200
+hits = 0
+clock()
+for i in range(1, DARTS):
+    x, y = random(), random()
+    dist = sqrt(x**2 + y**2)
+    if dist <= 1.0:
+        hits = hits + 1
+pi = 4 * (hits/DARTS)
+print("pi = %s" % pi)
+print("time = %-5.ess" % clock())
