b86075326a8eb6458dfd6be1246f0195cf69354e
cangxue <752787086@qq.com>
2017-08-29 01:36:35 +0800

    控制流修改

--- basic/control.py
+++ basic/control.py
@@ -36,8 +36,26 @@ print(sum)
 
 #while
 sum = 0
-n = 99
+n = 100
 while n > 0:
     sum = sum + n
     n = n - 1
 print(sum)
+
+#break
+n = 1
+while n <= 100:
+    if n > 10:
+        break
+    print(n)
+    n = n + 1
+print('END')
+
+#continue
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0:
+        continue
+    print(n)
+print('END')
