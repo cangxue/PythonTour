af3a0217f7c15da8f766ea66d5ae4f6b9780af9b
cangxue <752787086@qq.com>
2017-09-06 16:43:54 +0800

    filter 过滤函数学习

--- CXPythonTour/basic/func_program.py
+++ CXPythonTour/basic/func_program.py
@@ -74,3 +74,56 @@ def str_float(s):
     L = s.split('.',1)
     return reduce(fn, map(char_num, L[0])) + reduce(fx, map(char_num, L[1][-1::-1]))/10
 print('123.456')
+
+
+# filter
+def is_odd(n):
+    return n % 2 == 1
+L = list(filter(is_odd, [1, 2, 3, 4, 5, 6]))
+print(L)
+
+# 删除空字符串
+def not_empty(s):
+    return s and s.strip()
+L = list(filter(not_empty, ['A', '', 'B', None, 'C', '  ']))
+print(L)
+
+# 取素数
+# 1.这是一个生成器，并且是一个无限序列。
+def _odd_iter():
+    n = 1
+    while True:
+        n += 2
+        yield n
+
+# 2.定义一个筛选函数：
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+# 3.定义一个生成器，不断返回下一个素数：
+def primes():
+    yield 2;
+    it = _odd_iter() # 初始序列
+    while True:
+        n = next(it) # 返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n), it) # 构造新序列
+
+# 4.打印100以内的素数
+for n in primes():
+    if n < 100:
+        print(n)
+    else:
+        break
+
+# 回数
+def is_same(n):
+    a = str(n)
+    num = len(a)
+    result = True
+    for i in range(0, (num + 1) // 2):
+        if a[i] != a[num - 1 - i]:
+            result = False
+    return result
+
+print(list(filter(is_same, range(900, 1000))))
