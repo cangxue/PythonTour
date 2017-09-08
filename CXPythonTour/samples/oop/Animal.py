dc5580ad59e6e41074192838a2bf2d0b54a0d339
cangxue <752787086@qq.com>
2017-09-08 16:13:27 +0800

    对象属性及方法基本操作

--- CXPythonTour/samples/oop/Animal.py
+++ CXPythonTour/samples/oop/Animal.py
@@ -5,6 +5,9 @@
 
 class Animal(object):
 
+    def __init__(self):
+        self.x = 9
+
     def run(self):
         print('Animal is runing...')
 
@@ -44,3 +47,29 @@ run_twice(Cat())
 
 # 继承可以把父类的所有功能都直接拿过来，这样就不必重零做起，子类只需要新增自己特有的方法，也可以把父类不适合的方法覆盖重写。
 # 动态语言的鸭子类型特点决定了继承不像静态语言那样是必须的。
+
+
+#判断对象类型
+print(type(dog))
+
+a = Animal()
+b = Dog()
+c = Cat()
+print(isinstance(a, Dog))
+print(isinstance(c, Cat))
+
+# 获得一个对象的所有属性和方法，可以使用dir()函数
+print(dir(c))
+print(c.__str__())
+
+print(hasattr(a, 'x'))  # 有属性'x'吗？
+setattr(b, 'y', 5)  # 设置一个属性'y'
+print(hasattr(b, 'y'))  # 有属性'y'吗？
+print(getattr(b, 'y'))  # 有属性'y'吗？
+print(b.y)
+
+print(getattr(b, 'z', 404))  # 获取属性'z'，如果不存在，返回默认值404
+
+fn = getattr(b, 'run')  # 获得对象的方法，并赋值
+b.run()
+fn()
