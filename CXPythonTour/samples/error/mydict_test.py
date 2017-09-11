7184a97cb94d53ae81ac8f5d6684b0defb6fc23c
cangxue <752787086@qq.com>
2017-09-11 16:33:01 +0800

    单元测试

---
+++ CXPythonTour/samples/error/mydict_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 单元测试
+import unittest
+
+from mydict import Dict
+
+class TestDict(unittest.TestCase):
+
+    def setUp(self):
+        print('setUp...')
+
+    def tearDown(self):
+        print('tearDown...')
+
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+# 把mydict_test.py当做正常的python脚本运行
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+# python3 -m unittest mydict_test 执行测试
