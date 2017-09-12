b26b89bedc52c0168928ab401fef273807259f36
cangxue <752787086@qq.com>
2017-09-12 09:42:41 +0800

    json初步学习

---
+++ CXPythonTour/samples/io/use_json.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# JSON
+
+import json
+
+d = dict(name='Bob', age=20, score=88)
+
+print(json.dumps(d))
+
+
+json_str = '{"age": 20, "score": 88, "name": "Bob"}'
+d = json.loads(json_str)
+print(d)
+
+
+class Student(object):
+
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+    def __str__(self):
+        return 'Student object (%s, %s, %s)' % (self.name, self.age, self.score)
+
+
+s = Student('Bob', 20, 88)
+# 把任意class的实例变为dict：
+std_data = json.dumps(s, default=lambda obj: obj.__dict__)
+print('Dump Student:', std_data)
+rebuild = json.loads(std_data, object_hook=lambda d: Student(d['name'], d['age'], d['score']))
+print(rebuild)
