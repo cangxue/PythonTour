c7811c672f327539599593bca864f04e0f249904
cangxue <752787086@qq.com>
2017-11-02 14:39:33 +0800

    html转pdf编码实现

--- CXPythonTour/demo/爬虫/HTML-PDF.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# pip install requests 网络请求
-# pip install beautifulsoup4 操作html数据
-# pip install pdfkit  pdfkit 是 wkhtmltopdf 的Python封装包，html 到 pdf 的转换
