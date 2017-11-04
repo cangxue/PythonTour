45572412159835af5924d262c8a0cb1c2baea9b9
cangxue <752787086@qq.com>
2017-11-04 23:37:19 +0800

    创建单例类

--- CXPythonTour/design-mode/Singleton.py
+++ CXPythonTour/design-mode/Singleton.py
@@ -8,3 +8,14 @@ Created on 2017-11-03
 网址：https://yq.aliyun.com/articles/70418?spm=5176.100239.blogcont70448.17.Y8DcR1
 
 """
+
+import threading
+import time
+
+# 这里使用方法__new__来实现单例模式
+class Singleton(object): # 抽象单例
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            orig = super(Singleton, cls)
+            cls._instance = orig.__new__(cls, *args, **kwargs)
+        return cls._instance
