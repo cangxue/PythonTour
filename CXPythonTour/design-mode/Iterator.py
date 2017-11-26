2d9848d849a424083e25c74a19948e28e6254685
cangxue <752787086@qq.com>
2017-11-26 18:22:36 +0800

    提交

---
+++ CXPythonTour/design-mode/Iterator.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+"""
+Created on 2017-11-25
+@author: Palesnow
+
+功能：迭代器模式
+网址：https://yq.aliyun.com/articles/71068?utm_campaign=wenzhang&utm_medium=article&utm_source=QQ-qun&2017315&utm_content=m_13627
+
+实例：股票查询客户端
+
+"""
