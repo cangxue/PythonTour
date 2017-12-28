54de33636f3fb2ca77ed55e5d32787456ba16bf4
cangxue <752787086@qq.com>
2017-12-28 23:19:41 +0800

    豆瓣数据爬取

--- CXPythonTour/demo/Crawler/douban.py
+++ CXPythonTour/demo/Crawler/douban.py
@@ -21,9 +21,36 @@ class DOUBAN:
     def __init__(self, baseUrl):
         self.baseURL = baseUrl
 
-    def getPage:
+    def getPage(self):
         try:
-            response = requests.get(set.baseURL)
+            response = requests.get(self.baseURL)
+            if response.status_code == 200:
+                return response.text
+            return None
+        except requests.RequestException as e:
+            print(e)
+    def getContent(self, page):
+        pattern = re.compile('<table width=".*?<div class="pl2">.*?>(.*?)</a>.*?class="pl">(.*?)</p>'
+                             + '.*?<span class="rating_nums">(.*?)</span>.*?class="pl">(.*?)</span>', re.S)
+        items = re.findall(pattern, page)
+        for item in items:
+            yield {
+                'title': item[0].split("/")[0].strip(),
+                'actor': item[1].split("/"),
+                'average': item[2],
+                'content': item[3],
+            }
+    def start(self):
+        page = self.getPage()
+        for item in self.getContent(page):
+            print(item)
+
+
+url = 'https://movie.douban.com/tag/2016?start=' + str(0) + '&type=T'
+douban = DOUBAN(url)
+douban.start()
+
+
 
 
 
