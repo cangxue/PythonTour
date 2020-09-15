15377ccd99f6800b62cb821315097b265fce09be
xiaoma <752787086@qq.com>
2020-09-15 11:05:48 +0800

    基本数据类型

---
+++ CXPythonTour/samples/basic/basic.py
@@ -0,0 +1,31 @@
+
+# String
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+message = "  python "
+print(message)
+print(message.rstrip())
+
+# Number
+a = 2 + 3
+print(a)
+
+b = 0.2 + 0.1
+print(b)
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
--- CXPythonTour/samples/basic/helloworld.py
+++ CXPythonTour/samples/basic/helloworld.py
@@ -1,10 +1,17 @@
-
 # 直接打印
-print('Hello, world！')
+print("Hello Python interpreter!")
+
+print("Hello world!")
+
+message = "Hello Python world"
+print(message)
+
+message = "Hello Python Crash Course world"
+print(message)
 
 # 带输入的打印
-name = input('please enter your name:')
+# name = input('please enter your name:')
 
-age = input('please enter your age:')
+# age = input('please enter your age:')
 
-print(name + ' age is ' + age)
+# print(name + ' age is ' + age)
